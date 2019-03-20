f922de186b7bcea3c92e6332bc96abd406ee8ebb
alexandrusoloms <mariussolomes@gmail.com>
2019-03-20 12:25:45 +0000

    initial juce files for OriginalFx project

---
+++ OriginalFx/Builds/CLion/cmake-build-debug/CMakeFiles/SHARED_CODE.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/Source/PluginProcessor.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/Source/PluginEditor.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_audio_basics.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_audio_devices.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_audio_formats.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_audio_plugin_client_utils.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_audio_processors.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_audio_utils.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_core.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_cryptography.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_data_structures.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_events.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_graphics.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_gui_basics.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_gui_extra.cpp.o"
+  "CMakeFiles/SHARED_CODE.dir/home/mariussolomes/digital_audio_effects/OriginalFx/JuceLibraryCode/include_juce_opengl.cpp.o"
+  "libOriginalFx.pdb"
+  "libOriginalFx.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/SHARED_CODE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
